7b5a0c6d27bf869fc3473a5e0eea1fafe890a206
samuelGrontoski <contatosamuelgrontoski@gmail.com>
2023-09-05 10:28:37 -0300

    att lista04

--- Programação Orientada a Objetos/Exercícios/Lista_04/Lista_04_01/src/Rational.java
+++ Programação Orientada a Objetos/Exercícios/Lista_04/Lista_04_01/src/Rational.java
@@ -10,47 +10,72 @@ public class Rational {
     public Rational(int n, int d){
         numerador = n;
         denominador = d;
+
+        if (denominador > 1){
+            for (int i = 1; i <= denominador; i++){
+                if (numerador % i == 0 && denominador % i == 0){
+                    numerador = numerador / i;
+                    denominador = denominador / i;
+                }
+            }
+        }
     }
 
     public Rational somar(Rational r2){
-        Rational r3 = new Rational();
+        int n, d;
 
-        r3.denominador = numerador * r2.numerador;
-        r3.numerador = ((r3.denominador / denominador) * numerador) + ((r3.denominador / r2.denominador) * r2.numerador);
+        d = denominador * r2.denominador;
+        n = ((d / denominador) * numerador) + ((d / r2.denominador) * r2.numerador);
+
+        Rational r3 = new Rational(n, d);
 
         return r3;
     }
 
     public Rational subtrair(Rational r2){
-        Rational r3 = new Rational();
+        int n, d;
 
-        r3.denominador = numerador * r2.numerador;
-        r3.numerador = ((r3.denominador / denominador) * numerador) - ((r3.denominador / r2.denominador) * r2.numerador);
+        d = denominador * r2.denominador;
+        n = ((d / denominador) * numerador) - ((d / r2.denominador) * r2.numerador);
+
+        Rational r3 = new Rational(n, d);
 
         return r3;
     }
 
     public Rational multiplicar(Rational r2){
-        Rational r3 = new Rational();
+        int n, d;
 
-        r3.numerador = numerador * r2.numerador;
-        r3.denominador = denominador * r2.denominador;
+        n = numerador * r2.numerador;
+        d = denominador * r2.denominador;
+
+        Rational r3 = new Rational(n, d);
 
         return r3;
     }
 
     public Rational dividir(Rational r2){
-        Rational r3 = new Rational();
+        int n, d;
 
-        r3.numerador = numerador * r2.denominador;
-        r3.denominador = denominador * r2.numerador;
+        n = numerador * r2.denominador;
+        d = denominador * r2.numerador;
+
+        Rational r3 = new Rational(n, d);
 
         return r3;
     }
 
-    public void imprimir(Rational r){
-        String str = r.numerador + "/" + r.denominador;
+    public void imprimir(){
+        String str = numerador + "/" + denominador;
 
         System.out.println(str);
+    }
+
+    public void imprimirFloat(int n){
+        float flutuante;
+
+        flutuante = (float)numerador / (float)denominador;
+
+        System.out.printf("%." + n + "f", flutuante);
     }
 }
--- Programação Orientada a Objetos/Exercícios/Lista_04/Lista_04_01/src/usaRational.java
+++ Programação Orientada a Objetos/Exercícios/Lista_04/Lista_04_01/src/usaRational.java
@@ -1,3 +1,30 @@
 public class usaRational {
-    
+    public static void main(String[] argumentos){
+        Rational r1 = new Rational(2, 4);
+        Rational r2 = new Rational(3, 2);
+        Rational r3 = new Rational();
+
+        r1.imprimir();
+        r2.imprimir();
+
+        //somar
+        r3 = r1.somar(r2);
+        r3.imprimir();
+
+        //subtrair
+        r3 = r1.subtrair(r2);
+        r3.imprimir();
+
+        //multiplicar
+        r3 = r1.multiplicar(r2);
+        r3.imprimir();
+
+        //dividir
+        r3 = r1.dividir(r2);
+        r3.imprimir();
+
+        //imprimir ponto flutuante
+        r1.imprimirFloat(3);
+
+    }
 }
