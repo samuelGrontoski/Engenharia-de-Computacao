a5e976559b3d5b7428078cfa6e8065125e33f5fc
samuelGrontoski <contatosamuelgrontoski@gmail.com>
2023-11-28 10:35:24 -0300

    ajustes finais T2 - POO

--- Programação Orientada a Objetos/Trabalhos/Trabalhos 02 - Campeonato de Apostas em Jogos de Dados/src/JogarComoMaquina.java
+++ Programação Orientada a Objetos/Trabalhos/Trabalhos 02 - Campeonato de Apostas em Jogos de Dados/src/JogarComoMaquina.java
@@ -1,6 +1,6 @@
 public interface JogarComoMaquina {
     float valorAposta();
-    public int escolherJogo();
-    public void iniciarJogoAzar(float valorAposta, int rodada);
-    public void iniciarJogoGeneral(float valorAposta, int rodada);
+    int escolherJogo();
+    void iniciarJogoAzar(float valorAposta, int rodada);
+    void iniciarJogoGeneral(float valorAposta, int rodada);
 }
